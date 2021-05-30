273cac1afe770f995d5bd4f33e20c4855ac99c93
Fernando Herrera <fernando.herrera85@gmail.com>
2021-05-30 11:39:20 -0400

    Empty app

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'home',
+      initialRoute: 'perspective',
       routes: {
         'home': ( _ ) => HomeScreen(),
       },
