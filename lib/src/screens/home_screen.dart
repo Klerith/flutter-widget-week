ebd8dc8e03c4c20188817ceb304c67749a364302
Fernando Herrera <fernando.herrera85@gmail.com>
2021-05-30 12:22:30 -0400

    Fin del video ImageFilter

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -6,9 +8,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
+      body: Stack(
+        children: [
+
+          Center(
+              child: Image.network('https://miro.medium.com/max/3200/0*ZdwECz0chT1hOq6Y'),
+          ),
+
+          Positioned(
+            top: 350,
+            left: 20,
+            right: 20,
+            bottom: 380,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 5,
+                  sigmaY: 5
+                ),
+                child: Container(),
+              ),
+            ),
+          ),
+
+
+          Center(
+            child: Text(
+              'No blur aquí',
+              style: TextStyle( fontSize: 45, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          )
+
+
+        ],
+      )
+      
    );
   }
 }
